9895aec002357d3f758c871c03d602811e83ca0e
Xe-iu <xe_iu@outlook.com>
2026-03-24 23:07:01 +0800

    修复（删除“,”）

--- server/commands/peer/remove.py
+++ server/commands/peer/remove.py
@@ -14,7 +14,7 @@ def remove_peer(message):
     bot.send_message(
         message.chat.id,
         (
-        "This command has been disabled.\n该指令已被禁用。\n",
+        "This command has been disabled.\n该指令已被禁用。\n"
         f"If you need to peer with me, please go to the {config.CONTACT} channel and use the direct messaging feature to get in touch.\n"
         f"如需与我peer，请到 {config.CONTACT} 频道使用直通消息功能进行联系。"
         ),
