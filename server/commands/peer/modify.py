8977f999e2332755c9724aaaba9e754d0ae60143
Xe-iu <xe_iu@outlook.com>
2026-03-24 23:03:07 +0800

    尝试修复unsupported parse_mode

--- server/commands/peer/modify.py
+++ server/commands/peer/modify.py
@@ -13,11 +13,15 @@ from telebot.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemo
 @bot.message_handler(commands=["modify"], is_private_chat=True)
 def start_modify(message):
     bot.send_message(
-            message.chat.id,
-            "This command has been disabled.\n该指令已被禁用。\n",
-            f"If you need to peer with me, please go to the {config.CONTACT} channel and use the direct messaging feature to get in touch.\n"
-            f"如需与我peer，请到 {config.CONTACT} 频道使用直通消息功能进行联系。"
-        )
+        message.chat.id,
+        (
+        "This command has been disabled.\n该指令已被禁用。\n",
+        f"If you need to peer with me, please go to the {config.CONTACT} channel and use the direct messaging feature to get in touch.\n"
+        f"如需与我peer，请到 {config.CONTACT} 频道使用直通消息功能进行联系。"
+        ),
+        parse_mode="Markdown",
+        reply_markup=ReplyKeyboardRemove(),
+    )
     return
     step_manage("init", {}, None, message)
 
--- server/commands/user_manage/whoami.py
+++ server/commands/user_manage/whoami.py
@@ -8,11 +8,15 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeybo
 @bot.message_handler(commands=["whoami"], is_private_chat=True)
 def whoami(message, new_asn=None, info_node=None):
     bot.send_message(
-            message.chat.id,
-            "This command has been disabled.\n该指令已被禁用。\n",
-            f"If you need to peer with me, please go to the {config.CONTACT} channel and use the direct messaging feature to get in touch.\n"
-            f"如需与我peer，请到 {config.CONTACT} 频道使用直通消息功能进行联系。"
-        )
+        message.chat.id,
+        (
+        "This command has been disabled.\n该指令已被禁用。\n",
+        f"If you need to peer with me, please go to the {config.CONTACT} channel and use the direct messaging feature to get in touch.\n"
+        f"如需与我peer，请到 {config.CONTACT} 频道使用直通消息功能进行联系。"
+        ),
+        parse_mode="Markdown",
+        reply_markup=ReplyKeyboardRemove(),
+    )
     return
     if new_asn and message.chat.id not in db_privilege:
         if message.chat.id not in db:
